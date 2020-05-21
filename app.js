13414e5885a5643e4d5cd9d77c606f8b62d680d7
Vi Rey <gavialus@hotmail.com>
2020-05-21 14:17:23 +0000

    43. Crear una tarea persistente

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 
 // const argv = require('yargs').argv
 const argv = require ('./config/yargs').argv
+const porHacer = require('./por-hacer/por-hacer');
+
 
 console.log(argv);
 
@@ -9,7 +11,8 @@ let comando = argv._[0];
 
 switch(comando){
     case'crear':
-    console.log('Crear todas las tareas por hacer');
+       let tarea = porHacer.crear(argv.descripcion);
+       console.log(tarea)
     break;
     case'listar':
     console.log('Mostar todas las tareas por hacer');
